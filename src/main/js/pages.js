d5ca04e5df9e9ba93f44f396f64f7c156f665b91
Michael Ibanez <michael_ibanez@baylor.edu>
2018-09-26 16:04:01 -0500

    Added navbar to pages

--- src/main/js/pages.js
+++ src/main/js/pages.js
@@ -5,134 +5,173 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as Users from 'js/users';
 import * as Login from 'js/login';
-import * as Validation from 'js/alloy/utils/validation';
 import * as Bessemer from 'js/alloy/bessemer/components';
+
+export class NavBar1 extends React.Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="/#">Tempeturs Web App </a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent" >
+                    <ul className="nav navbar-nav navbn" >
+                        <li className="nav-item ">
+                            <a className="nav-link" href="/#"> Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/#/register"> Register </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/#/login"> Login </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/#/editprofile"> Profile </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/#/schedulesession"> Schedule </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/#/rate"> Rate </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/#/testendpoint"> test</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
 
 export class Home extends React.Component {
 	render() {
 		return (
-			<div className="container padded">
-				<h1> Welcome to the Tempeturs Web App Home Page! </h1>
-
-                <img src="https://img.huffingtonpost.com/asset/5b7fdeab1900001d035028dc.jpeg?cache=sixpwrbb1s&ops=1910_1000" height="225" width="430"/>
-
-                <div>
-					<div>
-						<Bessemer.Button><Link to="/register" style={{color: '#FFF'}}>Register Now!</Link></Bessemer.Button>
-					</div>
-					<div>
-						<h6>OR</h6>
-					</div>
-					<div>
-						<Bessemer.Button><Link to="/login" style={{color: '#FFF'}}>Login</Link></Bessemer.Button>
-					</div>
+			<div>
+				<NavBar1/>
+				<div style={{float:'center',paddingRight:'25%', paddingLeft:'25%'}} >
+					<h1 > Welcome to the Tempeturs Web App Home Page! </h1>
+					<img style={{float:'center'}} src="https://img.huffingtonpost.com/asset/5b7fdeab1900001d035028dc.jpeg?cache=sixpwrbb1s&ops=1910_1000" height="225" width="430"/>
 				</div>
-
-				<ul>
-					<li><Link to="/editprofile">Edit Profile</Link></li>
-					<li><Link to="/schedulesession">Schedule Session</Link></li>
-					<li><Link to="/rate">Rate</Link></li>
-                    <li><Link to="/testendpoint">Test</Link></li>
-				</ul>
-
-
 			</div>
 		);
 	}
 }
 
 export class RegisterPage extends React.Component {
-	render() {
-		return (
-			<div className="container padded">
-				<div className="row">
-					<div className="col-6 offset-md-3">
-						<h2>Register</h2>
-						<hr />
-						<Login.RegistrationForm />
-						<hr />
+    render() {
+        return (
+            <div>
+                <NavBar1/>
+                <div className="container padded">
+                    <div className="row">
+                        <div className="col-6 offset-md-3">
+                            <h2>Register</h2>
+                            <hr />
+                            <Login.RegistrationForm />
+                            <hr />
 
-                        { _.isDefined(this.props.user) &&
-                        <div> You are registered, {this.props.user.principal}!</div>
-                        }
+                            { _.isDefined(this.props.user) &&
+                            <div> You are registered, {this.props.user.principal}!</div>
+                            }
 
-					</div>
-				</div>
-			</div>
-		);
-	}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export class LoginPage extends React.Component {
-	render() {
-		return (
-			<div className="container padded">
-				<div className="row">
-					<div className="col-6 offset-md-3">
-						<h2>Login</h2>
-						<hr />
-						<Login.LoginForm />
-					</div>
-				</div>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div>
+                <NavBar1/>
+                <div className="container padded">
+                    <div className="row">
+                        <div className="col-6 offset-md-3">
+                            <h2>Login</h2>
+                            <hr />
+                            <Login.LoginForm />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 class EditProfile extends React.Component {
-	render() {
-		return (
-			<div className="container padded">
-				<p>Edit your profile here.</p>
+    render() {
+        return (
+            <div>
+                <NavBar1/>
+                <div className="container padded">
+                    <p>Edit your profile here.</p>
 
-				{ _.isDefined(this.props.authentication) &&
-				<div>{this.props.authentication['access_token']}</div>
-				}
+                    { _.isDefined(this.props.authentication) &&
+                    <div>{this.props.authentication['access_token']}</div>
+                    }
 
-				{ _.isDefined(this.props.user) &&
-				<div>Welcome, {this.props.user.principal}!</div>
-				}
-			</div>
-		);
-	}
+                    { _.isDefined(this.props.user) &&
+                    <div>Welcome, {this.props.user.principal}!</div>
+                    }
+                </div>
+            </div>
+        );
+    }
 }
 
 
 
 EditProfile = connect(
-	state => ({
-		authentication: Users.State.getAuthentication(state),
-		user: Users.State.getUser(state)
-	})
+    state => ({
+        authentication: Users.State.getAuthentication(state),
+        user: Users.State.getUser(state)
+    })
 )(EditProfile);
 
 export { EditProfile };
 
 export class ScheduleSession extends React.Component {
-	render() {
-		return (
-			<div className="container padded">
-				Schedule your session here.
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div>
+                <NavBar1/>
+                <div className="container padded">
+                    Schedule your session here.
+                </div>
+            </div>
+        );
+    }
 }
 
 export class Rate extends React.Component {
-	render() {
-		return (
-			<div className="container padded">
-				Rate your sitters here.
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div>
+                <NavBar1/>
+                <div className="container padded">
+                    Rate your sitters here.
+                </div>
+            </div>
+        );
+    }
 }
 
 export class TestEndpoint extends React.Component {
     render() {
         return (
-            <div className="container padded">
-                Testing an endpoint.
+            <div>
+                <NavBar1/>
+                <div className="container padded">
+                    Testing an endpoint.
+                </div>
             </div>
         );
     }
