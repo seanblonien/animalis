31e314620b43da9ef1669d6fa08518840de1fa59
Sean Blonien <sean_blonien@baylor.edu>
2018-10-02 14:42:12 -0500

    removed unused service functions

--- src/main/java/petfinder/site/common/user/UserService.java
+++ src/main/java/petfinder/site/common/user/UserService.java
@@ -62,96 +62,12 @@ public class UserService {
 			return principal;
 		}
 
-		public void setPrincipal(String principal) {
-			this.principal = principal;
-		}
-
 		public String getPassword() {
 			return password;
 		}
 
 		public void setPassword(String password) {
 			this.password = password;
-		}
-
-		public String getFname() {
-			return fname;
-		}
-
-		public void setFname(String fname) {
-			this.fname = fname;
-		}
-
-		public String getLname() {
-			return lname;
-		}
-
-		public void setLname(String lname) {
-			this.lname = lname;
-		}
-
-		public String getPhone() {
-			return phone;
-		}
-
-		public void setPhone(String phone) {
-			this.phone = phone;
-		}
-
-		public String getAddress() {
-			return address;
-		}
-
-		public void setAddress(String address) {
-			this.address = address;
-		}
-
-		public String getCity() {
-			return city;
-		}
-
-		public void setCity(String city) {
-			this.city = city;
-		}
-
-		public String getState() {
-			return state;
-		}
-
-		public void setState(String state) {
-			this.state = state;
-		}
-
-		public String getZip() {
-			return zip;
-		}
-
-		public void setZip(String zip) {
-			this.zip = zip;
-		}
-
-		public String getPetSitter() {
-			return petSitter;
-		}
-
-		public void setPetSitter(String petSitter) {
-			this.petSitter = petSitter;
-		}
-
-		public String getPetOwner() {
-			return petOwner;
-		}
-
-		public void setPetOwner(String petOwner) {
-			this.petOwner = petOwner;
-		}
-
-		public String getEmailNotifications() {
-			return emailNotifications;
-		}
-
-		public void setEmailNotifications(String emailNotifications) {
-			this.emailNotifications = emailNotifications;
 		}
 
 		public Map<String, Object> getAttributes() {
