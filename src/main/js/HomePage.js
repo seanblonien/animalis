165a056cfa4e74d3972112e93b44dd7a45b7bc65
jmeyre1998 <John_Eyre@baylor.edu>
2018-10-11 16:10:49 -0500

    Added setup for JUnit testing

--- src/main/js/HomePage.js
+++ src/main/js/HomePage.js
@@ -1,24 +1,50 @@
 import React from 'react';
+import * as Users from 'js/users';
+import {connect} from 'react-redux';
+import _ from 'lodash';
 
 class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
     render() {
-
         return (
-            <div style={{
-                float: 'center', marginRight: '25%', marginLeft: '25%', marginTop: '25px',
-                borderRadius: '20px', backgroundColor: '#D5D5D5'
-            }}>
-                <h1 style={{float: 'center', marginRight: '5%', marginLeft: '5%', marginTop: '5px'}}> Welcome to the
-                    Tempeturs Web App Home Page! </h1>
-                <div style={{width: '100%',textAlign:'center'}}>
-                    <img style={{borderRadius: '25px', padding: '2%'}}
-                        src="https://img.huffingtonpost.com/asset/5b7fdeab1900001d035028dc.jpeg?cache=sixpwrbb1s&ops=1910_1000"
-                         height="255" width="430"/>
+            <div>
+                {_.isDefined(this.props.user) && (this.props.user.roles.includes('OWNER')) &&
+                <div>
+                    <h3>Owner Home Page</h3>
+                </div>
+                }
+
+                {_.isDefined(this.props.user) && (this.props.user.roles.includes('SITTER')) &&
+                <div>
+                    <h3>Sitter Home Page</h3>
+                </div>
+                }
+
+                <div style={{
+                    float: 'center', marginRight: '25%', marginLeft: '25%', marginTop: '25px',
+                    borderRadius: '20px', backgroundColor: '#D5D5D5'
+                }}>
+                    <h1 style={{float: 'center', marginRight: '5%', marginLeft: '5%', marginTop: '5px'}}> Welcome to the
+                        Tempeturs Web App Home Page! </h1>
+                    <div style={{width: '100%',textAlign:'center'}}>
+                        <img style={{borderRadius: '25px', padding: '2%'}}
+                            src="https://img.huffingtonpost.com/asset/5b7fdeab1900001d035028dc.jpeg?cache=sixpwrbb1s&ops=1910_1000"
+                             height="255" width="430"/>
+                    </div>
                 </div>
             </div>
         );
     }
 }
+
+HomePage = connect(
+    state => ({
+        authentication: Users.State.getAuthentication(state),
+        user: Users.State.getUser(state)
+    })
+)(HomePage);
 
 export default HomePage;
---
+++ src/test/java/petfinder/site/test/unit/Testing.java
@@ -0,0 +1,18 @@
+package petfinder.site.test.unit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import petfinder.site.common.pet.PetDto;
+import petfinder.site.common.user.UserDao;
+import petfinder.site.common.user.UserDto;
+import petfinder.site.common.user.UserService;
+
+public class Testing {
+
+    @Test
+    public void testSomething() {
+
+    }
+
+}
