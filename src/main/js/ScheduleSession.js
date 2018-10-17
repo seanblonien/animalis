68f717564dc1caa6e88249ace2cfbad37089eb86
Jack Titzman <jrt0799@att.net>
2018-10-17 11:45:14 -0500

    Updated schedule session page to better reflect the number of fields that will appear on the screen. Still need to convert to more user-friendly options.

--- src/main/js/ScheduleSession.js
+++ src/main/js/ScheduleSession.js
@@ -34,23 +34,49 @@ class ScheduleSession extends React.Component {
 
         return (
             <form name="name" onSubmit={handleSubmit(form => this.onSubmit(form))}>
-                <Bessemer.Field name="startdate" friendlyName="Start Date" placeholder="never"
+                <Bessemer.Field name="startday" friendlyName="Start Day" placeholder="29"
                                 validators={[Validation.requiredValidator]} />
 
-                <Bessemer.Field name="starttime" friendlyName="Start Time" placeholder="never"
+                <Bessemer.Field name="startmonth" friendlyName="Start Month" placeholder="November"
                                 validators={[Validation.requiredValidator]} />
 
-                <Bessemer.Field name="enddate" friendlyName="End Date" placeholder="never"
+                <Bessemer.Field name="startyear" friendlyName="Start Year" placeholder="2018"
                                 validators={[Validation.requiredValidator]} />
 
-                <Bessemer.Field name="endtime" friendlyName="End Time" placeholder="never"
+                <Bessemer.Field name="starthour" friendlyName="Start Hour" placeholder="3"
                                 validators={[Validation.requiredValidator]} />
 
-                <Bessemer.Field name={'ownerLocation'} friendlyName={'Location of owner'}
+                <Bessemer.Field name="startminute" friendlyName="Start Minute" placeholder="00"
+                                validators={[Validation.requiredValidator]} />
+
+                <Bessemer.Field name="startampm" friendlyName="Start AM/PM" placeholder="PM"
+                                validators={[Validation.requiredValidator]} />
+
+                <Bessemer.Field name="endday" friendlyName="End Day" placeholder="2"
+                                validators={[Validation.requiredValidator]} />
+
+                <Bessemer.Field name="endmonth" friendlyName="End Month" placeholder="December"
+                                validators={[Validation.requiredValidator]} />
+
+                <Bessemer.Field name="endyear" friendlyName="End Year" placeholder="2018"
+                                validators={[Validation.requiredValidator]} />
+
+                <Bessemer.Field name="endhour" friendlyName="End Hour" placeholder="10"
+                                validators={[Validation.requiredValidator]} />
+
+                <Bessemer.Field name="endminute" friendlyName="End Minute" placeholder="30"
+                                validators={[Validation.requiredValidator]} />
+
+                <Bessemer.Field name="endampm" friendlyName="End AM/PM" placeholder="AM"
+                                validators={[Validation.requiredValidator]} />
+
+                <label>Location of Sitting:</label>
+
+                <Bessemer.Field name={'ownerLocation'} friendlyName={'My address'}
                                 onChange={this.handleCheckboxChange}
                                 field={<input type="checkbox" value={this.state.checkedItems.get('petOwner')} />} />
 
-                <Bessemer.Field name={'sitterLocation'} friendlyName={'Location of sitter'}
+                <Bessemer.Field name={'sitterLocation'} friendlyName={'Sitter\'s address'}
                                 onChange={this.handleCheckboxChange}
                                 field={<input type="checkbox" value={this.state.checkedItems.get('petOwner')} />} />
 
