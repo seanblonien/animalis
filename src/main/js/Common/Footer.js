6cbffdd71b769dd5c37b365041b67352ab1e1195
Sean Blonien <sean_blonien@baylor.edu>
2018-11-27 15:16:09 -0600

    added contact us

--- src/main/js/Common/Footer.js
+++ src/main/js/Common/Footer.js
@@ -15,6 +15,7 @@ export default class Footer extends React.Component {
                     <div className="col-md-5 justify-content-between footer-links">
                         <div className="grow px-1"><a href="/#/about">About</a></div>
                         <div className="grow px-1"><a href="/#/support">Support and FAQ</a></div>
+                        <div className="grow px-1"><a href="/#">Contact Us</a></div>
                     </div>
                 </div>
             </div>
