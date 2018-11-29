cfe4df6b105216f01ad4ce5e9a9b002da2b9bdac
jmeyre1998 <John_Eyre@baylor.edu>
2018-11-29 14:49:13 -0600

    Added About page

--- src/main/js/Common/Pages.js
+++ src/main/js/Common/Pages.js
@@ -363,13 +363,31 @@ export class AboutPage extends React.Component {
         return (
             <div>
                 <div className="container padded">
-                    <div className="row horizontal-center">
-                        <h2 className="col">About</h2>
-                    </div>
-                    <div>
+					<div className="row horizontal-center">
+						<h2 className="col">About</h2>
+					</div>
+					<div>
+						<h4>Our Vision:</h4>
+						<p>&emsp;Animalis is a service to make pet owners' busy lives easier. Pet owners and pet sitters
+							come to Animalis for an easy, quick way to connect. Animalis offers 4 types of
+							sessions to pet owners. These include: sitting, boarding, daycare, and drop-in. This wide
+							array of services is sure to fit your needs. That is why we are confident Animalis is the
+							best service available for your fluffy friends!
+						</p>
 
-                    </div>
-                </div>
+						<h4>How it Works:</h4>
+						<p>&emsp;Owners post jobs for pet sitters to browse. If a pet sitter wants to work a job, they bid on
+							it. If the pet owner accepts their bid, the job is set!
+						</p>
+					</div>
+					<div>
+						<h4>Meet The Team:</h4>
+						<p>&emsp;- Sean Blonien</p>
+						<p>&emsp;- John Eyre</p>
+						<p>&emsp;- Jacob Sembroski</p>
+						<p>&emsp;- Jack Titzman</p>
+					</div>
+				</div>
             </div>
         );
     }
